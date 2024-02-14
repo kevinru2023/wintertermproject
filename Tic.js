aeb43f566e7270bb50f0c5fff83c517b0e744c52
Kevin Rubio <kevin@10-249-143-170.wireless.oregonstate.edu>
2024-02-14 01:07:31 -0800

    whole lot of html and css changes plus new home page thanks to knorad, and changed logic of js thanks to chatgpt for telling me i'm wrong in scope stuff

--- Tic.js
+++ Tic.js
@@ -1,80 +1,82 @@
 document.addEventListener("DOMContentLoaded", function(event){
 {
     let win  = false; 
-    let currentplayer = "x"
-    let s1 = 0, s2 = 0, s3 = 0, s4 = 0, s5 = 0, s6 =0 , s7 = 0, s8 = 0, s9 = 0; 
-    //these are variables containing the informatin needed for the squares 
-    //so if s = 0 then no player has clicked it, if s = 1 player X has clicked it, if s = 2 then player O has clicked it
+    let currentplayer = "x";
+    let squares = [0,0,0,
+                   0,0,0,
+                   0,0,0];
+    //had to change these to an array due to reference issues and js not having pointers shoutout to chatgpt for that ngl 
+    //so if s[0] = 0 then no player has clicked it, if s[0] = 1 player x has clicked it, if s[0] = 2 then player o has clicked it
 
     
-    function checkwin(x)
+    function checkwin(arr)
     {
-        if (s1 == s2 && s1 == s3)
+        if(arr[0] == arr[1] == arr[2])
         {
-            x = true 
+            win = true; 
         }
-        else if (s4 == s5 && s4 == s6)
+        else if(arr[3] == arr[4]== arr[5])
         {
-            x = true 
+            win = true; 
         }
-        else if (s7 == s8 && s7 == s9)
+        else if(arr[6] == arr[7]== arr[8])
         {
-            x = true 
+            win = true; 
         }
-        else if (s1 == s4 && s1 == s7)
+        else if(arr[0] == arr[3]== arr[6])
         {
-            x = true 
+            win = true; 
         }
-        else if (s2 == s5 && s2 == s8)
+        else if(arr[1] == arr[4]== arr[7])
         {
-            x = true 
+            win = true; 
         }
-        else if (s3 == s6 && s3 == s9)
+        else if(arr[2] == arr[5]== arr[8])
         {
-            x = true 
+            win = true; 
         }
-        else if (s1 == s5 && s1 == s9)
+        else if(arr[0] == arr[4]== arr[8])
         {
-            x = true 
+            win = true; 
         }
-        else if (s3 == s5 && s3 == s7)
+        else if(arr[2] == arr[4]== arr[6])
         {
-            x = true 
+            win = true; 
         }
-        else{
-            x = false 
-        }
-        return x 
+        
+        else;
+            win = false; 
+        
 
     }
     
-    
-    
-    
-    function change(button, square)
+    // things to do still, make it so the win function is called and print winners, and also fix image sizing 
+    //heavily changed code here by the help of chat gpt because scope in javascript is stupid, and I can't belive im saying this but c pointers and memeory is clutch
+    function change(index)
     {
-        let bttn = document.getElementById(button) 
-        if(square == 0 && win != true)
+        let bttn = document.getElementById("b" + (index+1));
+        console.log(bttn)
+        if(squares[index] == 0 && win != true)
         {
-            console.log("if executed")
             if(currentplayer == "x")
             {
                 console.log("x player")
-                bttn.style.backgroundImage="chainsaws.jpg"; 
-                square = 1; 
+                bttn.style.backgroundImage="chainsaws.jpg";
+                squares[index] = 1; 
                 currentplayer = "o"; 
             }
             else if(currentplayer == "o")
             {
                 console.log("o player")
                 bttn.style.backgroundImage="log.jpg"; 
-                square = 2; 
+                squares[index] = 2; 
                 currentplayer = "x"; 
             }
             else
             {
                 return 1; 
             }
+
         } 
         return 0; 
             
@@ -82,23 +84,23 @@ document.addEventListener("DOMContentLoaded", function(event){
     }
     
     document.getElementById("b1").addEventListener("click",() => {
-        change("b1", s1)}) ;
+        change(0)}) ;
     document.getElementById("b2").addEventListener("click",() => {
-        change("b2", s2)}); 
+        change(1)}); 
     document.getElementById("b3").addEventListener("click",() => {
-        change("b3", s3)}); 
+        change(2)}); 
     document.getElementById("b4").addEventListener("click",() => {
-        change("b4", s4)}); 
+        change(3)}); 
     document.getElementById("b5").addEventListener("click",() => {
-        change("b5", s5)}); 
+        change(4)}); 
     document.getElementById("b6").addEventListener("click",() => {
-        change("b6", s6)}); 
+        change(5)}); 
     document.getElementById("b7").addEventListener("click",() => {
-        change("b7", s7)}); 
+        change(6)}); 
     document.getElementById("b8").addEventListener("click",() => {
-        change("b8", s8)}); 
+        change(7)}); 
     document.getElementById("b9").addEventListener("click",() => {
-        change("b9", s9)}); 
+        change(8)}); 
     
 }
 })
