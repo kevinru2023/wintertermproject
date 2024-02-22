7e45d89924cd284e8f68bae23a3ffc6d6ced6d96
Kevin Rubio <kevin@10-249-143-170.wireless.oregonstate.edu>
2024-02-22 11:00:46 -0800

    started working heavily on pong game, still hate javascript

--- pong.js
+++ pong.js
@@ -1,5 +1,39 @@
 const canvas = document.getElementById("gamearea"); 
 const ctx = canvas.getContext("2d"); 
+ //alot of what I am using is thanks to this tutorial in a different progrmaming language: https://www.instructables.com/Pong-With-Processing/ 
+ //I'm also using a youtube tutorial that I was recommended to understand how to draw in js: https://www.youtube.com/watch?v=UUFPEgRKwf4
 
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0 , canvas.width, canvas.height);
+ //initializing our game screen 
+ function clearScreen(){
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0 , canvas.width, canvas.height);
+}
+
+//game loop 
+function drawGame(){
+    clearScreen(); 
+}
+
+//all things related to the ball, ps I hate javascript. 
+class ballClass{
+    constructor(tempX, tempY, tempDiameter){
+        this.x = tempX; 
+        this.y = tempY; 
+        this.speedX = 0; 
+        this.speedY = 0; 
+        this.diameter = tempDiameter; 
+    }
+    draw(){
+        ctx.fillStyle= "white"; 
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.diameter / 2, 0, Math.PI*2 );
+        ctx.fill();
+    }
+    move(){
+        this.y += this.speedY; 
+        this.x += this.speedX; 
+    }
+
+}
+
+drawGame();
